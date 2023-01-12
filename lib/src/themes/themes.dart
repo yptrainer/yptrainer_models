e247e3268c181b3e6f853a8ff2385faf7f22dfd2
federicocecconi <ghiande-boe0a@icloud.com>
2023-01-12 14:45:59 +0100

    added themes

---
+++ lib/src/themes/themes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+TextTheme appTextTheme = TextTheme(
+  bodySmall: TextStyle(fontSize: 13, fontWeight: FontWeight.w300),
+  bodyMedium: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+  labelMedium: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
+  titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+  titleMedium: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+  titleSmall: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+);
